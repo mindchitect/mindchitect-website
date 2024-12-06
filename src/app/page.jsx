c4f23a4bbbd3905d322c6263e2b9b496613f8512
Brandon Awan <abibangbrandon855@gmail.com>
2024-12-06 20:44:20 +0100

    copy update on landing page and setting of autodeploy

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,11 +11,10 @@ export default function page() {
 			<section className="h-[55vh] md:h-[40vh] lg:h-[90vh] bg-[url(/hero.png)] bg-cover bg-center pt-[5rem] flex flex-col items-center justify-center w-full">
 				<div className="flex flex-col gap-y-5 items-center justify-center w-full">
 					<h1 className="font-extrabold text-white text-[4rem] md:text-[6rem] lg:text-[7rem] xl:text-[8rem] shadow-lg leading-[100%] text-center md:w-[60%]">
-						Mission-critical digital solutions
+					Empowering Minds Through Technology
 					</h1>
 					<p className="mt-2 text-[2rem] md:w-[60%] lg:w-[55%] xl:w-[40%] text-white text-center font-semibold">
-						High performing system solutions that is customized to your
-						organization.
+					Mindchitect: Your partner for cutting-edge software development, impactful training, and robust cybersecurity solutions.
 					</p>
 					<button className="h-[60px] w-[220px] bg-[#EEBC07] hover:bg-[#f0ca3f] transition-all duration-200 rounded-full text-[1.7rem] font-semibold ">
 						Get Started
@@ -28,11 +27,7 @@ export default function page() {
 						Who we are
 					</h2>
 					<p className="text-[1.8rem] mt-3 w-[90%]">
-						Coregrade provides robust IT solution, system integration especially
-						where performance is critical, custom software development, data
-						security and infrastructure services. We make IT solutions easy to
-						have and run securely, ensuring high performance at reduced
-						production cost.
+					Mindchitect is a leading provider of comprehensive IT solutions, empowering businesses and individuals with cutting-edge software development, impactful coding bootcamps, and expert cybersecurity consulting. We deliver innovative solutions that drive growth and success.
 					</p>
 					<button className="mt-5 h-[60px] w-[220px] bg-primary hover:bg-[#010D30] text-white  transition-all duration-200 rounded-full text-[1.7rem] font-semibold ">
 						Learn more
@@ -62,10 +57,7 @@ export default function page() {
 								</h3>
 							</div>
 							<p className="text-[1.7rem] font-medium mt-5 text-center">
-								We provide design and software development services for startups
-								and enterprise level companies from the idea stage, through
-								UX/UI design, programming, dev-ops, release, consultancy, and
-								aftercare.
+							Custom software solutions tailored to meet your business needs. From concept to deployment, we deliver seamless user experiences with cutting-edge technology.
 							</p>
 							<button className="mt-10 h-[60px] w-[220px] bg-primary hover:bg-[#010D30] text-white  transition-all duration-200 rounded-full text-[1.7rem] font-semibold ">
 								Read More
@@ -76,13 +68,10 @@ export default function page() {
 						<div className="lg:w-[300px] xl:w-[350px] h-[450px] rounded-2xl drop-shadow-2xl bg-white border-[1.5px] border-gray-100 p-8 flex flex-col items-center justify-center">
 							<div className="flex flex-col gap-y-4 items-center justify-center">
 								<SystemIntegration />
-								<h3 className="text-[2.5rem] font-bold">System Integration</h3>
+								<h3 className="text-[2.5rem] font-bold">Coding Bootcamp</h3>
 							</div>
 							<p className="text-[1.7rem] font-medium mt-5 text-center">
-								We provide design and software development services for startups
-								and enterprise level companies from the idea stage, through
-								UX/UI design, programming, dev-ops, release, consultancy, and
-								aftercare.
+							Empower your career with our intensive coding bootcamps. Learn the latest programming skills and frameworks to succeed in a competitive tech landscape.
 							</p>
 							<button className="mt-10 h-[60px] w-[220px] bg-primary hover:bg-[#010D30] text-white  transition-all duration-200 rounded-full text-[1.7rem] font-semibold ">
 								Read More
@@ -93,14 +82,10 @@ export default function page() {
 						<div className="lg:w-[300px] xl:w-[350px] h-[450px] rounded-2xl drop-shadow-2xl bg-white border-[1.5px] border-gray-100 p-8 flex flex-col items-center justify-center">
 							<div className="flex flex-col gap-y-4 items-center justify-center">
 								<CyberSecurityIcon />
-								<h3 className="text-[2.5rem] font-bold">Data Security</h3>
+								<h3 className="text-[2.5rem] font-bold">Cybersecurity</h3>
 							</div>
 							<p className="text-[1.7rem] font-medium mt-5 text-center">
-								We provide design and software development services for startups
-								and enterprise level companies from the idea stage, through
-								UX/UI design, programming, dev-ops, release, consultancy, and
-								aftercare.
-							</p>
+							We strengthen your security posture with comprehensive penetration testing and expert advice. Our team will identify vulnerabilities, assess risks, and provide actionable security tips to safeguard your systems and data.							</p>
 							<button className="mt-10 h-[60px] w-[220px] bg-primary hover:bg-[#010D30] text-white  transition-all duration-200 rounded-full text-[1.7rem] font-semibold ">
 								Read More
 							</button>
@@ -111,11 +96,10 @@ export default function page() {
 			<section className="px-8 md:px-[40px] lg:px-[60px] xl:px-[90px] bg-[#202684] py-16 flex flex-col md:flex-row justify-center items-center">
 				<div className="md:w-1/2">
 					<h2 className="text-[3.5rem] md:text-[4rem] lg:text-[5rem] font-bold text-white">
-						We love building meaningful and useful software products.
+					Crafting Innovative Software Solutions that Drive Impact.
 					</h2>
 					<p className="text-[1.8rem] mt-10 w-[90%] text-white ">
-						Disruptive and innovative products in the education, private and
-						public sector.
+					We deliver cutting-edge solutions that empower organizations across education, private, and public sectors to achieve their full potential.
 					</p>
 				</div>
 				<Image
